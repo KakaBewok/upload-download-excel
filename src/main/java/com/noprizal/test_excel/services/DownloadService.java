b1bbe51043c4b28254aa6564f562c8d7dddbdd11
KakaBewok <rizalnov667@gmail.com>
2024-07-24 19:12:34 +0700

    fixing

--- src/main/java/com/noprizal/test_excel/services/DownloadService.java
+++ src/main/java/com/noprizal/test_excel/services/DownloadService.java
@@ -2,10 +2,11 @@ package com.noprizal.test_excel.services;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.util.List;
 
-import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,33 +20,27 @@ public class DownloadService {
 	@Autowired
 	private StudentRepository studentRepo;
 
-	public ByteArrayInputStream downloadExcelData() {
-		List<Student> students = studentRepo.findAll();
+	 public ByteArrayInputStream loadExcel() throws IOException {
+	        List<Student> dataList = studentRepo.findAll();
 
-		Workbook workbook = new XSSFWorkbook();
-		Sheet sheet = workbook.createSheet("student");
+	        Workbook workbook = new XSSFWorkbook();
+	        Sheet sheet = workbook.createSheet("Data");
 
-		Row headerRow = sheet.createRow(0);
-		headerRow.createCell(0).setCellValue("Name");
-		headerRow.createCell(1).setCellValue("Age");
+	        Row headerRow = sheet.createRow(0);
+	        headerRow.createCell(0).setCellValue("Name");
+	        headerRow.createCell(1).setCellValue("Age");
 
-		Integer rowIndex = 1;
-		for (Student student : students) {
-			Row row = sheet.createRow(rowIndex++);
-			
-//			row.createCell(0)
-//			row.createCell(0)
-//			row.createCell(0)
-//			row.createCell(0)
-//			row.createCell(0)
-//			row.createCell(0)
-		}
-		//(id, name, nim, faculty, major, gpa)
+	        int rowIndex = 1;
+	        for (Student data : dataList) {
+	            Row row = sheet.createRow(rowIndex++);
+//	            row.createCell(0).setCellValue(data.getName());
+//	            row.createCell(1).setCellValue(data.getAge());
+	        }
 
-		ByteArrayOutputStream out = new ByteArrayOutputStream();
-//		workbook.write(out);
-//		workbook.close();
+	        ByteArrayOutputStream out = new ByteArrayOutputStream();
+	        workbook.write(out);
+	        workbook.close();
 
-		return new ByteArrayInputStream(out.toByteArray());
-	}
+	        return new ByteArrayInputStream(out.toByteArray());
+	    }
 }
